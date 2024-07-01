a3fec33b58d6432d907f1e818cbe470f11fa60f1
Rocklin <rocklin2603@gmail.com>
2024-07-01 14:42:26 +0530

    update script modified

--- polybar/scripts/updates.sh
+++ polybar/scripts/updates.sh
@@ -1,47 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-BAR_ICON="󰁟 "
-NOTIFY_ICON="󱠇 "
+if ! updates=$(checkupdates 2> /dev/null | wc -l ); then
+    updates=0
+fi
 
-get_total_updates() { UPDATES=$(checkupdates 2>/dev/null | wc -l); }
-
-while true; do
-    get_total_updates
-
-    # notify user of updates
-    if hash notify-send &>/dev/null; then
-        if (( UPDATES > 50 )); then
-            notify-send -u critical -i $NOTIFY_ICON \
-                "You really need to update!!" "$UPDATES New packages"
-        elif (( UPDATES > 25 )); then
-            notify-send -u normal -i $NOTIFY_ICON \
-                "You should update soon" "$UPDATES New packages"
-        elif (( UPDATES > 2 )); then
-            notify-send -u low -i $NOTIFY_ICON \
-                "$UPDATES New packages"
-        fi
-    fi
-
-    # when there are updates available
-    # every 10 seconds another check for updates is done
-    while (( UPDATES > 0 )); do
-        if (( UPDATES == 1 )); then
-            echo " $UPDATES"
-        elif (( UPDATES > 1 )); then
-            echo " $UPDATES"
-        else
-            echo $BAR_ICON
-        fi
-        sleep 10
-        get_total_updates
-    done
-
-    # when no updates are available, use a longer loop, this saves on CPU
-    # and network uptime, only checking once every 30 min for new updates
-    while (( UPDATES == 0 )); do
-        echo $BAR_ICON
-        sleep 1800
-        get_total_updates
-    done
-done
-
+if [ "$updates" -gt 0 ]; then
+    echo "  $updates"
+else
+    echo "󰁟 "
+fi
